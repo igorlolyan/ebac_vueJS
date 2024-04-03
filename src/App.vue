94a4db61fb966a801e4eb499c735dd1e1f4ad14e
igorlolyan <igor.lol.yan@gmail.com>
2024-04-03 11:40:23 -0300

    Corrigindo function

--- src/App.vue
+++ src/App.vue
@@ -59,28 +59,28 @@ const cadastraTarefa = () => {
     <header class="p-5 mb-4 mt-4 bg-light rounded-3">
       <h1>Minhas tarefas</h1>
       <p>
-        Você possui {{ getTarefasPendentes().length }} tarefas penentes
+        Você possui {{ getTarefasPendentes().length }} tarefas pendentes
       </p>
     </header>
     <form @submit.prevent="cadastraTarefa">
       <div class="row">
         <div class="col">
-          <input :value="estado.tarefaTemp" @change="evento => estado.tarefaTemp = evento.target.value" required type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
-        </div>
+            <input :value="estado.tarefaTemp" @change="evento => estado.tarefaTemp = evento.target.value" required type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+          </div>
+          <div class="col-md-2">
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+          </div>
         <div class="col-md-2">
-          <button type="submit" class="btn btn-primary">Cadastrar</button>
+          <select @change="evento => estado.filtro = evento.target.value" class="form-control">
+            <option value="Todas">Todas tarefas</option>
+            <option value="Pendentes">Pendentes</option>
+            <option value="Finalizadas">Finalizadas</option>
+          </select>
         </div>
-      </div>
-      <div class="col-md-2">
-        <select @change="evento => estado.filtro = evento.target.value" class="form-control">
-          <option value="Todas">Todas tarefas</option>
-          <option value="Pendentes">Pendentes</option>
-          <option value="Finalizadas">Finalizadas</option>
-        </select>
       </div>
     </form>
     <ul class="list-group mt-4">
-      <li class="list-group-item" v-for="tarefa in getTarefasFiltradas">
+      <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
         <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
         <label :class="{done: tarefa.finalizada}" class="ms-3" :for="tarefa.titulo">
           {{tarefa.titulo}}
